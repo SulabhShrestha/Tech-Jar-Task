11d5960d82d978a56ddbfe83c1ba569cc285e8b8
Sulabh Shrestha <shresthasulabh777@gmail.com>
2023-12-07 23:55:45 +0545

    UserPage: code refactored

--- lib/views/user_page/pages/all_user_display_page.dart
+++ lib/views/user_page/pages/all_user_display_page.dart
@@ -1,37 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:tech_jar/models/user_model.dart';
-import 'package:tech_jar/view_models/user_view_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tech_jar/providers/all_users_provider.dart';
 
-class AllUserDisplayPage extends StatelessWidget {
+class AllUserDisplayPage extends ConsumerWidget {
   const AllUserDisplayPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final allUsers = ref.watch(allUsersProvider).allUsers;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('All user'),
       ),
-      body: FutureBuilder<List<UserModel>>(
-          future: UserViewModel().getAllUser(),
-          builder: (_, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            if (snapshot.hasError) {
-              return const Center(child: Text("Error"));
-            }
-
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (_, index) {
-                return ListTile(
-                  leading: const Icon(Icons.person),
-                  title: Text(snapshot.data![index].username),
-                  subtitle: Text(snapshot.data![index].email),
-                );
-              },
-            );
-          }),
+      body: ListView.builder(
+        itemCount: allUsers.length,
+        itemBuilder: (_, index) {
+          return ListTile(
+            leading: const Icon(Icons.person),
+            title: Text(allUsers[index].username),
+            subtitle: Text(allUsers[index].email),
+          );
+        },
+      ),
     );
   }
 }
